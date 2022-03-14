38d65848e44b5f1296f565a5e5be0dd913b1a5b8
fbonnafous31 <francois.bonnafous@hotmail.fr>
2022-03-14 15:51:39 +0100

    init project

---
+++ tests/RomanTest.php
@@ -0,0 +1,17 @@
+<?php
+
+    namespace App;
+
+use PHPUnit\Framework\TestCase;
+
+use function PHPUnit\Framework\assertEquals;
+
+    class RomanTest extends TestCase {
+
+        public function testPhpunit(){
+            assertEquals('1', '1');
+        }
+
+    }
+
+?>
