52c2a89cae0ce12d31267a917c3ac2a99146b0f4
fbonnafous31 <francois.bonnafous@hotmail.fr>
2022-03-15 10:54:42 +0100

    form test

--- index.php
+++ index.php
@@ -1,11 +1,30 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 <?php
 
-    $string = 'Bonjour';
-    echo $string.'<br>';
-    echo $string[4].'<br>';
-    
-    for($i = 0; $i < strlen($string); $i++) {
-	    echo $string[$i].'<br>';
+use App\Roman;
+
+require_once ('src/Roman.php');
+
+    if (isset ($_POST['input_roman'])) {
+        $value = $_POST['input_roman'];
+        $roman = new Roman();
+        $result = $roman->translate($value);
     }
 
 ?>
+
+<div class="container">
+    <form action="index.php" method="POST">
+        <div class="form-group">
+            <input type="text" name="input_roman" id="input_roman" class="form-control" placeholder="Entrer une valeur" value="<?= $value ?>">
+        </div>
+        <button type="submit" class="btn btn-primary">Envoyer</button>
+    </form>  
+
+    <?php if (!is_null($result)): ?>
+        <div class="alert alert-success">
+            <?= $result ?>
+        </div>
+    <?php endif ?>
+</div>
